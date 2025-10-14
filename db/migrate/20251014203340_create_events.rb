59780ea75c3ea2bd38ddc8a09889a57f332bffb8
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-14 14:35:07 -0600

    feat: add Event model generator with files
    - migration: update foreign key from Event to User
    because :host isn't a table name
    - tell migration to look for the user table

---
+++ db/migrate/20251014203340_create_events.rb
@@ -0,0 +1,11 @@
+class CreateEvents < ActiveRecord::Migration[8.0]
+  def change
+    create_table :events do |t|
+      t.datetime :date
+      t.string :location
+      t.references :host, null: false, foreign_key: { to_table: :users }
+
+      t.timestamps
+    end
+  end
+end
