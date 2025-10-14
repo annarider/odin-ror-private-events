341b607b483078d160205afedd17988caf270543
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-14 14:40:04 -0600

    feat: add Event model validations to check DQ

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
   # Handles relationship when a user created an event
   belongs_to :host, class_name: 'User', foreign_key: 'host_id'
+
+  validates :date, :location, presence: true
+  validates :date, comparison: { greater_than: -> { Time.current } }, on: :create
 end
