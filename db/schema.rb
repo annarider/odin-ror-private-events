b5bcec9238dfff02d28d84a7ba09a2ec9e5e3c78
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-15 16:36:47 -0600

    feat: run rails generate controller Users show

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_15_192055) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_15_221243) do
   create_table "events", force: :cascade do |t|
     t.datetime "date"
     t.string "location"
-    t.integer "host_id", null: false
+    t.integer "creator_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["host_id"], name: "index_events_on_host_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,5 +32,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_15_192055) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "events", "users", column: "host_id"
+  add_foreign_key "events", "users", column: "creator_id"
 end
