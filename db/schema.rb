b715af05b212cf09e72ad6aa4b061f36b7aa2e58
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-15 14:26:12 -0600

    feat: run db:migrate after Devise generated User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_14_203340) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_15_192055) do
   create_table "events", force: :cascade do |t|
     t.datetime "date"
     t.string "location"
@@ -18,6 +18,18 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_14_203340) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["host_id"], name: "index_events_on_host_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "events", "users", column: "host_id"
