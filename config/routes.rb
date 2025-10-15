23da104313c3a751e2fc7bda9d3422e53c1ee554
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-15 16:53:22 -0600

    fix: remove redundant "get" after adding resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get "users/show"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
@@ -15,4 +14,5 @@ Rails.application.routes.draw do
   root "events#index"
 
   resources :events, only: [:index]
+  resources :users, only: [:show]
 end
