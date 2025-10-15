c8e4d272231a77f068e1617aab5af03caec1e054
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-15 17:33:59 -0600

    feat: add #create to EventsController + routes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,20 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+
+    if @event.save
+      redirect_to events_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:date, :location)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "events#index"
 
-  resources :events, only: [:index]
+  resources :events, only: [:index, :new, :create]
   resources :users, only: [:show]
 end
