3f7aeda1bf7bf96746b6be32c6bde4cebf5e36bb
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-16 13:19:56 -0600

    feat: add Invitations controller create & destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "invitations/create"
+  get "invitations/destroy"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
