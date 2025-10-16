6b7e2eb48fb460dd0125dc5402fe3b29531f9d1d
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-16 13:26:56 -0600

    feat: add logic to Invitations#create controller

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,7 +1,21 @@
 class InvitationsController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :destroy]
+
   def create
+    @invitation = current_user.invitations.build(invitation_params)
+
+    if @invitation.save
+      redirect_to invitations_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  def destroy
   end
 
-  def destroy
+  private
+
+  def invitation_params
+    params.require(:invitation).permit(:status)
   end
 end
