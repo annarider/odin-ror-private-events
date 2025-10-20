bb3a9ecf939d12881413c8725d33ec57f20154c2
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-20 10:19:16 -0600

    fix: remove date validation on Event model
    - validation makes it not allow creating a past
    event
    - past event is required for project feature
    to separate list of events from past and future
    events

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,6 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
 
   validates :date, :location, presence: true
-  validates :date, comparison: { greater_than: -> { Time.current } }, on: :create
 
   has_many :invitations
   has_many :attendees, through: :invitations, source: :attendee
