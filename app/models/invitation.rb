9e022514d88477958bb602a46015b7e0a5dd8d37
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-20 13:54:05 -0600

    feat: add validation to Invitation model

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,4 +1,7 @@
 class Invitation < ApplicationRecord
   belongs_to :attendee, class_name: 'User', foreign_key: 'user_id'
   belongs_to :attended_event, class_name: 'Event', foreign_key: 'event_id'
+
+  validates :attendee, uniqueness: { scope: :attended_event_id, 
+                                     message: 'has already been invited to this event'}
 end
