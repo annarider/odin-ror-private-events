6d1abc17b1072a687cb8bcd047bfe3ca179a63d5
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-20 13:54:41 -0600

    feat: add nested invitations resources in routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,6 @@ Rails.application.routes.draw do
   root "events#index"
 
   resources :events, only: [:index, :new, :create, :show]
-  resources :invitations, only: [:create, :destroy]
+    resources :invitations, only: [:create, :new, :destroy]
   resources :users, only: [:show]
 end
