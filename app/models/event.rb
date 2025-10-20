c2a9989e4c76406c39118767ad1be5bc1948afd0
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-20 12:38:37 -0600

    feat: add class method queries to Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,4 +6,12 @@ class Event < ApplicationRecord
 
   has_many :invitations
   has_many :attendees, through: :invitations, source: :attendee
+
+  def self.past
+    where('date <= ?', Time.now)
+  end
+
+  def self.upcoming
+    where('date > ?', Time.now)
+  end
 end
