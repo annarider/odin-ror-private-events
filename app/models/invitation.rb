fd257af51f0800b2e769301d5f51a9f5b1bda82a
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-21 10:28:16 -0600

    feat: add enum to Invitation model for status

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -2,6 +2,8 @@ class Invitation < ApplicationRecord
   belongs_to :attendee, class_name: 'User', foreign_key: 'user_id'
   belongs_to :attended_event, class_name: 'Event', foreign_key: 'event_id'
 
+  enum :status, { pending: 0, accepted: 1, declined: 2 }, default: :pending
+
   validates :attendee, uniqueness: { scope: :attended_event_id, 
                                      message: 'has already been invited to this event'}
 end
