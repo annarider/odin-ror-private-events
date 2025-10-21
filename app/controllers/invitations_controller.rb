c2e15505fea1180fb6cfa3ec3adcb4eefd38a00a
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-21 10:37:02 -0600

    feat: add authorization & resource scoping
    - InvitationsController adds additional security
    checks
    - `before_action :set_event` loads the event first
    (makes sure the invitation child
    only belongs to the event parent)
    - `authorize_creator!` (only the event creator can invite people)
    - `set_invitation` (loads invitations through the
    parent events)
    - this supports nested resource authorization
    - changing event_id to user_id focuses on the
    invitation pertains to which user am I inviting
    versus which event am I inviting myself to (this was
    the previous open behavior where users could invite
    themselves to any event)

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,5 +1,8 @@
 class InvitationsController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :destroy]
+  before_action :authenticate_user!
+  before_action :set_event
+  before_action :authorize_creator!, only: [:new, :create]
+  before_action :set_invitation, only: [:destroy]
 
   def create
     @invitation = current_user.invitations.build(invitation_params)
@@ -19,7 +22,21 @@ class InvitationsController < ApplicationController
 
   private
 
+  def set_event
+    @event = Event.find(params[:event_id])
+  end
+
+  def authorize_creator!
+    unless @event.creator?(current_user)
+      redirect_to root_path, alert: 'Only the event creator can invite users.'
+    end
+  end
+
+  def set_invitation
+    @invitation = @event.invitations.find(params[:id])
+  end
+
   def invitation_params
-    params.require(:invitation).permit(:event_id)
+    params.require(:invitation).permit(:user_id)
   end
 end
